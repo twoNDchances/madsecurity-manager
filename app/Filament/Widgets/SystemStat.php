73ebffa80dfa2fd76dc97f803f08a2bb67ad8983
Cao Xuan Thien <caoxuanthien13122002@gmail.com>
2025-08-12 17:02:45 +0700

    update 12/08/2025

--- app/Filament/Widgets/SystemStat.php
+++ app/Filament/Widgets/SystemStat.php
@@ -3,6 +3,7 @@
 namespace App\Filament\Widgets;
 
 use App\Models\Defender;
+use App\Models\Report;
 use App\Services\FilamentWidgetService;
 use Carbon\Carbon;
 use Filament\Widgets\StatsOverviewWidget as BaseWidget;
@@ -17,6 +18,8 @@ class SystemStat extends BaseWidget
         return [
             self::getActivationDefenders(),
             self::getApplicationGroups(),
+            self::getImplementationDecisions(),
+            self::getReportsRecieved(),
         ];
     }
 
@@ -46,5 +49,31 @@ class SystemStat extends BaseWidget
             "$changedToday changes today"
         )
         ->color('success');
+    }
+
+    private static function getImplementationDecisions()
+    {
+        $totalAssignmentDecisions = DB::table('defenders_decisions')->count();
+        $applicationDecisions = DB::table('defenders_decisions')->where('status', true)->count();
+        $changedToday = DB::table('defenders_decisions')->whereDate('updated_at', Carbon::today())->count();
+        return self::$widget::stat(
+            'Implementation Decisions ',
+            "$applicationDecisions/$totalAssignmentDecisions",
+            'heroicon-o-scale',
+            "$changedToday changes today"
+        )
+        ->color('danger');
+    }
+
+    private static function getReportsRecieved()
+    {
+        $reports = Report::where('created_at', Carbon::today())->count();
+        return self::$widget::stat(
+            'Reports Recieved',
+            $reports,
+            'heroicon-o-flag',
+            'Today',
+        )
+        ->color('warning');
     }
 }
